f3f7ab62b1a245ddf0de247883c6901d25dce74b
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-27 08:31:29 +1000

    adding dependencies and class App with initial state

--- employee_directory_react/src/App.js
+++ employee_directory_react/src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from '../public/img/logo.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+class App extends Component {
+  state = {
+    isLoading: true,
+    employees: [],
+    employeesForDisplay: [],
+    searchKeyword: "",
+    sortNameAsc: true,
+    sortEmailAsc: true,
+    sortDOBAsc: true,
+    sortPhoneAsc: true
+  }
+
+  render (){
+    return (
+      <div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,9 +25,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
