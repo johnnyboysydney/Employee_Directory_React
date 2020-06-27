7f3a931fdc5f6527145cae282329774a9e317c14
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 09:42:15 +1000

    added Wrapper to App

--- employee_directory_react/src/App.js
+++ employee_directory_react/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './CSS/App.css';
-import Main from "./components/Main/index";
-import Header from "./components/Header/index";
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <br></br>
-      <Main />
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <br></br>
+        <Main />
+      </Wrapper>
     </div>
-  )
+  );
 }
 
 export default App;
