6f78d9f29179c3f58c49686f36c30fd96165e3e1
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 09:19:08 +1000

    modified the div by adding a form tag

--- employee_directory_react/src/components/Search.js
+++ employee_directory_react/src/components/Search.js
@@ -3,13 +3,18 @@ import "../CSS/Search.css";
 
 const Search = (props) => {
     return (
-        <div>
-            Search
-            <div>
-                <input className="form-control" type="text" {...props} />
-            </div>
+        <div className="searchBox">
+            <form className="form-inline">
+                <input
+                className="form-control"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                onChange={event => handleSearchChange(event)}
+                />
+            </form>
         </div>
-    )
-};
+    );
+}
 
 export default Search;
