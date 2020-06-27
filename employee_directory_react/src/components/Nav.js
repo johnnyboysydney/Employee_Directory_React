5b279eb007181ac5283ed26d7ef2b633f21b72dc
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 09:29:03 +1000

    adding wrapper js and setting up Nav js

---
+++ employee_directory_react/src/components/Nav.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import Search from "./Search.js";
+import "../CSS/Search.css";
+
+function Nav({ handleSearchChange }) {
+    return (
+        <nav>
+            <div>
+                <Search handleSearchChange = {handleSearchChange} />
+            </div>
+        </nav>
+        );
+    }
+
+export default Nav;
