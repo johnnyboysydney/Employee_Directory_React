9d983e4b5a9cceceaba32a0ecee5f77cd4d79f67
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 09:38:21 +1000

    working on dataArea

---
+++ employee_directory_react/src/components/DataArea.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import DataTable from "./DataTable";
+import Nav from "./Nav";
+import API from "../utils/API";
+import "../CSS/DataArea.css";
+
+export default class DataArea extends Component {
+
+}
