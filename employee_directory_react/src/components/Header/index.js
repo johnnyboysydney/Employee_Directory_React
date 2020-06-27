5f2fed8d1ce7df8f628328c96f19f85ef02dbe6b
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 07:59:19 +1000

    correcting name of CSS file

--- employee_directory_react/src/components/Header/index.js
+++ employee_directory_react/src/components/Header/index.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import { Jumbotron } from "reactstrap";
+import "../../CSS/header.css";
+
+const Header = (props) => {
+    return (
+        <div>
+            <Jumbotron>
+                <h1 className="display-3">Employee Directory</h1>
+                <p className="content">Click on up-down arrow to sort Last Names A-Z or Z-A. Use the search box to narrow your results.</p>
+            </Jumbotron>
+        </div>
+    )
+}
