cb68a226f9436b739e051f334f26d1fa98a6b31b
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 09:32:22 +1000

    adding Main js

---
+++ employee_directory_react/src/components/Main.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from "react";
+import DataArea from "./DataArea";
+import "../CSS/Main.css";
+
+export default class Main extends Component {
+    render() {
+        return (
+            <>
+                <DataArea />
+            </>
+        );
+    };
+}
