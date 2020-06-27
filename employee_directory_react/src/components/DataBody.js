0f3b5796ab1123133bc44fe40dcaf525fe9047a4
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 09:36:18 +1000

    initial funtion for date format

--- employee_directory_react/src/components/DataBody.js
+++ employee_directory_react/src/components/DataBody.js
@@ -1,5 +1,15 @@
 import React from "react";
 import "../styles/DataBody.css";
 
+function DataBody({ users }) {
+    function formatDate(date) {
+      const dateArray = date.split("-");
+      const year = dateArray[0];
+      const month = dateArray[1];
+      const dayArray = dateArray[2].split("T");
+      const day = dayArray[0];
+      const formattedDate = [month, day, year].join("-");
+      return formattedDate;
+    }
 
 export default DataBody;
