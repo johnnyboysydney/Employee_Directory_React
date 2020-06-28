9571ef6a40cc9607b07f3b256c98f70255fe1303
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 23:08:54 +1000

    adding build

---
+++ build/precache-manifest.3de3af507aa017a79086d6462ec94b40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b1b9b70c8c3cd130f68c00f0fd4fd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1787e952fbfa28a3b318",
+    "url": "/static/css/main.58299658.chunk.css"
+  },
+  {
+    "revision": "6ccfc8dbc46c969b7c49",
+    "url": "/static/js/2.5ead97fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ead97fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1787e952fbfa28a3b318",
+    "url": "/static/js/main.b90a8c64.chunk.js"
+  },
+  {
+    "revision": "2c9419d678ae78901d60",
+    "url": "/static/js/runtime-main.9eb745ac.js"
+  }
+]);
