30928c306d53209405baec28a2ca3c9f86e77499
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 16:36:17 +1000

    added working working files

--- employee_directory_react/src/components/DataBody.js
+++ employee_directory_react/src/components/DataBody.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/DataBody.css";
+import "../CSS/DataBody.css";
 
 function DataBody({ users }) {
     function formatDate(date) {
@@ -11,5 +11,40 @@ function DataBody({ users }) {
       const formattedDate = [month, day, year].join("-");
       return formattedDate;
     }
-
+    return (
+        <tbody>
+          {users[0] !== undefined && users[0].name !== undefined ? (
+            users.map(({ login, name, picture, phone, email, dob }) => {
+              return (
+                <tr key={login.uuid}>
+                  <td data-th="Image" className="align-middle">
+                    <img
+                      src={picture.medium}
+                      alt={"profile image for " + name.first + " " + name.last}
+                      className="img-responsive"
+                    />
+                  </td>
+                  <td data-th="Name" className="name-cell align-middle">
+                    {name.first} {name.last}
+                  </td>
+                  <td data-th="Phone" className="align-middle">
+                    {phone}
+                  </td>
+                  <td data-th="Email" className="align-middle">
+                    <a href={"mailto:" + email} target="__blank">
+                      {email}
+                    </a>
+                  </td>
+                  <td data-th="DOB" className="align-middle">
+                    {formatDate(dob.date)}
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <></>
+          )}
+        </tbody>
+    );
+}
 export default DataBody;
