01d0f98ac04625c99cc531b1a6ab87cacd507158
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 20:17:47 +1000

    adding build to repo

---
+++ employee_directory_react/build/precache-manifest.b6f55fd3b785a1a8b93869468dbd78d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486f98e4adac4dcb0ddcca9acf0eba9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec63793270f8f77d9700",
+    "url": "/static/css/main.58299658.chunk.css"
+  },
+  {
+    "revision": "6ccfc8dbc46c969b7c49",
+    "url": "/static/js/2.5ead97fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ead97fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec63793270f8f77d9700",
+    "url": "/static/js/main.d3c49273.chunk.js"
+  },
+  {
+    "revision": "2c9419d678ae78901d60",
+    "url": "/static/js/runtime-main.9eb745ac.js"
+  }
+]);
