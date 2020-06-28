319196c619a5c9f66065f3488ed29f64ce7a2ab1
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 16:19:17 +1000

    refactored code

--- employee_directory_react/src/components/Header.js
+++ employee_directory_react/src/components/Header.js
@@ -4,9 +4,10 @@ import "../CSS/Header.css";
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                    <h1 className="display-3">Employee Directory</h1>
-                    <p className="content">Click on up-down arrow to sort Last Names A-Z or Z-A. Use the search box to narrow your results.</p>
+            <div className="header">
+                    <h1>Employee Directory</h1>
+                    <hr></hr>
+                    <p>Click on up-down arrow to sort Last Names A-Z or Z-A. Use the search box to narrow your results.</p>
             </div>
         )
     };    
