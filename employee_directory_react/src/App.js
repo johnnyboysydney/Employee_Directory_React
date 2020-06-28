6ae69967f9f13aeca9ca3fcbf4d2c25873ca4e36
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 16:33:54 +1000

    added footer

--- employee_directory_react/src/App.js
+++ employee_directory_react/src/App.js
@@ -3,15 +3,20 @@ import './CSS/App.css';
 import Main from "./components/Main";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="page-container">
+      <div>
       <Wrapper>
         <Header />
         <br></br>
         <Main />
+        <br></br>
       </Wrapper>
+    </div>
+      <Footer />
     </div>
   );
 }
