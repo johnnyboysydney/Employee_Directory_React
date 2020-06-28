3783d3e3604c673b06d54c7b517b4b94536c5131
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-28 18:14:14 +1000

    added functionality with links

---
+++ employee_directory_react/src/components/Links.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../CSS/Links.css";
+
+const Links = () => {
+    return (
+        <div className="links">
+            <a href="#footer">Footer</a> <span>|</span> <a href="https://github.com/johnnyboysydney/Employee_Directory_React" target="_blank">GitHub</a> <span>|</span> <a href="https://github.com/johnnyboysydney/Employee_Directory_React/blob/master/LICENSE" target="_blank">Copyright</a>
+        </div>
+    )
+}
+
+export default Links;
